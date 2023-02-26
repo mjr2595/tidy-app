49b7b5da81d2b4b5c040db953872d83cf03b92fc
mjr2595 <michaeljohnraymond@gmail.com>
2023-02-26 15:32:55 -0600

    icons and header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,34 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { UserIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "Tidy!",
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row p-4 items-center mx-1 space-x-2">
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/2088/2088675.png",
+          }}
+          className="h-7 w-7 p-4"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-xl">Time to Tidy!</Text>
+        </View>
+        <UserIcon size={35} color="black" />
+      </View>
+    </SafeAreaView>
   );
 };
 
